c90b6e1574c2f90bc3b44737177d3822f1073a09
Mary Oliveira <maryolivr@gmail.com>
2025-09-19 18:07:58 -0300

    Adicionando função excluir amigo

--- js/app.js
+++ js/app.js
@@ -3,13 +3,18 @@ let amigos = [];
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
+
     amigos.push(amigo.value);
+
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
         lista.textContent = lista.textContent + ' , ' + amigo.value;
     }
     amigo.value = '';
+
+    atualizarLista();
+    atualizarSorteio();
 }
 
 function sortear() {
@@ -26,6 +31,13 @@ function sortear() {
 
     }
 }
+
+function excluirAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+    atualizarSorteio();
+}
+
 function embaralha(lista) {
 
     for (let indice = lista.length; indice; indice--) {
@@ -35,6 +47,31 @@ function embaralha(lista) {
         //atribuição via destructuring
         [lista[indice - 1], lista[indiceAleatorio]] = 
            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        // Cria um elemento de parágrafo para cada amigo
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = amigos[i];
+       
+        // Adiciona um evento de clique para excluir o amigo
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+
+        // Adiciona o parágrafo à lista
+        lista.appendChild(paragrafo);
     }
 }
 
